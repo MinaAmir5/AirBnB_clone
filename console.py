1d9857153501b8a72cf5576551c839d36c65f754
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-11-12 05:49:53 +0200

    update

--- console.py
+++ console.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Entry point of the command interpreter """
 import cmd
-from models import storage
-from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
+from models import storage
 from models.city import City
 from models.amenity import Amenity
 from models.state import State
+from models.place import Place
 from models.review import Review
+from models.base_model import BaseModel
 import json
 import shlex
 
