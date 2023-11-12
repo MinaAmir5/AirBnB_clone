27e21bb6a438dc1916b1c9528a17ff85f0bdbda2
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-11-12 20:25:56 +0200

    update

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
 """
-Unittest for amenity.py
+Unitest for amenity.py
 """
 import unittest
-from models.amenity import Amenity
 import datetime
+from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
-    """Tests instances and methods from amenity class"""
+    """Tests instnces and methos from amnity class"""
 
     a = Amenity()
 
-    def test_class_exists(self):
-        """tests if class exists"""
-        res = "<class 'models.amenity.Amenity'>"
-        self.assertEqual(str(type(self.a)), res)
+    def testHasAttributes(self):
+        """verify if attribtes exist"""
+        self.assertTrue(hasattr(self.a, 'created_at'))
+        self.assertTrue(hasattr(self.a, 'id'))
+        self.assertTrue(hasattr(self.a, 'updated_at'))
+        self.assertTrue(hasattr(self.a, 'name'))
 
     def test_user_inheritance(self):
-        """test if Amenity is a subclass of BaseModel"""
+        """test if Amenty is a suclass of BaseModel"""
         self.assertIsInstance(self.a, Amenity)
 
-    def testHasAttributes(self):
-        """verify if attributes exist"""
-        self.assertTrue(hasattr(self.a, 'name'))
-        self.assertTrue(hasattr(self.a, 'id'))
-        self.assertTrue(hasattr(self.a, 'created_at'))
-        self.assertTrue(hasattr(self.a, 'updated_at'))
-
     def test_types(self):
-        """tests if the type of the attribute is the correct one"""
-        self.assertIsInstance(self.a.name, str)
-        self.assertIsInstance(self.a.id, str)
+        """tests if the tye of the atribute is the crrect one"""
         self.assertIsInstance(self.a.created_at, datetime.datetime)
+        self.assertIsInstance(self.a.id, str)
         self.assertIsInstance(self.a.updated_at, datetime.datetime)
+        self.assertIsInstance(self.a.name, str)
+
+    def test_class_exists(self):
+        """tests if class eists"""
+        res = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.a)), res)
 
 if __name__ == '__main__':
     unittest.main()
